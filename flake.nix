fc3aa9c10081aa090d5df95353426544cae3b073
snqre <marco.bizxaro@gmail.com>
2026-06-18 22:09:36 +0200

    update flake

--- flake.nix
+++ flake.nix
@@ -12,8 +12,112 @@
 			"aarch64-linux"
 			"aarch64-darwin"
 		];
-		
+
 		perSystem = { config, pkgs, system, ... }: {
+			checks.pytest = pkgs.python312.pkgs.buildPythonPackage {
+				pname = "endpoint-tests";
+				version = "0.1.0";
+				format = "other";
+				src = ./.;
+				
+				propagatedBuildInputs = [
+					pkgs.python312.pkgs.fastapi
+					pkgs.python312.pkgs.httpx
+					pkgs.python312.pkgs.uvicorn
+					pkgs.python312.pkgs.websockets
+					pkgs.python312.pkgs.wsproto
+
+					config.packages.endpoint-openpgp-sequoia
+				];
+
+				buildInputs = [
+					pkgs.nettle
+					pkgs.gmp
+					pkgs.openssl
+				];
+
+				nativeCheckInputs = [
+					pkgs.coreutils
+					pkgs.python312.pkgs.pytestCheckHook
+					pkgs.python312.pkgs.pytest-asyncio
+					pkgs.python312.pkgs.trustme
+				];
+
+				pytestFlags = [
+					"-vv"
+					"--endpoint-trace"
+				];
+
+				preCheck = ''
+					export PATH="${config.packages.endpoint-openpgp-sequoia}/bin:$PATH"
+					export SEQUOIA_BINARY_PATH="${config.packages.endpoint-openpgp-sequoia}/bin/endpoint-openpgp-sequoia"
+				'';
+						
+				dontBuild = true;
+				dontInstall = true;
+			};
+			
+			packages.endpoint-openpgp-sequoia = pkgs.rustPlatform.buildRustPackage {
+				pname = "endpoint-openpgp-sequoia";
+				version = "0.1.0";
+
+				src = ./openpgp-sequoia;
+				cargoHash = "sha256-d1RZYUGO3Nnl9J19OWqdhMWbqeijgYcp77SsRAu+XzI=";
+
+				nativeBuildInputs = [
+					pkgs.pkg-config
+				];
+
+				buildInputs = [
+					pkgs.nettle
+					pkgs.gmp
+					pkgs.openssl
+				];
+			};
+
+			packages.endpoint = pkgs.python312.pkgs.buildPythonApplication {
+				pname = "endpoint-protocol";
+				version = "0.1.0";
+				format = "pyproject";
+
+				src = ./.;
+
+				nativeBuildInputs = [
+					pkgs.python312.pkgs.setuptools
+					pkgs.python312.pkgs.wheel
+					pkgs.python312.pkgs.pythonRelaxDepsHook
+				];
+
+				pythonRelaxDeps = [
+					"fastapi"
+					"wsproto"
+				];
+				
+				pythonRemoveDeps = [
+					"endpoint-openpgp-sequoia"
+				];
+
+				doCheck = false;
+
+				propagatedBuildInputs = [
+					pkgs.python312.pkgs.fastapi
+					pkgs.python312.pkgs.httpx
+					pkgs.python312.pkgs.uvicorn
+					pkgs.python312.pkgs.websockets
+					pkgs.python312.pkgs.wsproto
+
+					config.packages.endpoint-openpgp-sequoia
+				];
+
+				buildInputs = [
+					pkgs.nettle
+					pkgs.gmp
+					pkgs.openssl
+				];
+				
+				meta.mainProgram = "endpoint";
+			};
+
 			devShells.default = pkgs.mkShell {
 				nativeBuildInputs = [
 					pkgs.nixd
@@ -24,10 +128,6 @@
 					pkgs.rust-analyzer
 					pkgs.lld
 					pkgs.pkg-config
-					
-					pkgs.nettle
-					pkgs.gmp
-					
 					(pkgs.python312.withPackages (pkgs: [
 						pkgs.fastapi
 						pkgs.httpx
@@ -39,13 +139,11 @@
 						pkgs.trustme
 					]))
 				];
-				
-				buildInputs = [
-					pkgs.openssl
-				];
-				
+
 				shellHook = ''
-					
+					export PKG_CONFIG_PATH="${pkgs.openssl.dev}/lib/pkgconfig:${pkgs.nettle}/lib/pkgconfig"
+					export PATH="${config.packages.endpoint-openpgp-sequoia}/bin:$PATH"
+					export SEQUOIA_BINARY_PATH="${config.packages.endpoint-openpgp-sequoia}/bin/endpoint-openpgp-sequoia"
 				'';
 			};
 		};
